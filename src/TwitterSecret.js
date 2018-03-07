6e678dacfabd0a4e568ae2ba09760677f94f9e79
Vijaysuryawanshi1993 <suryawanshivijay2011@gmail.com>
2018-03-07 12:23:35 +0530

    gitignore change

--- src/TwitterSecret.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    twitter: {
-        appId: 'ZsdX6RfnqN9uwAwDAh79vlId7',
-        appSecret: 'CqblUE1JQZg8f8MQb9L8QJgqMjqlJtKU7Oqcq5iM8AnWjlDSGL',
-        callback: 'https://demoapp.com',
-    }
-};
